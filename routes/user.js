3910df210fbe511fc11bedb310a7e489a33bf7c7
Odiovock <64711277+Odiovock@users.noreply.github.com>
2025-04-29 20:56:52 -0400

    userRouter complete

--- routes/user.js
+++ routes/user.js
@@ -46,11 +46,41 @@ userRouter.get("/:id", async (req, res) => {
 });
 
 userRouter.put("/:id", async (req, res) => {
+    const params = req.body;
 
+    let query = "UPDATE users SET ";
+    let i = 1;
+    for (const param in params) {
+        if (param === "first_name" || param === "last_name" || param === "email" || param === "phone")
+        {
+            if (i > 1)  {
+                query += ",";
+            }
+            query += `${param}=$${i} `;
+            i++;
+        }
+    }
+    query += `WHERE id=$${i}`;
+
+    const paramList = [...Object.values(params), req.params.id];
+
+    try {
+        const result = await pool.query(query, paramList);
+        res.status(204).send();
+    } catch (error) {
+        console.error(error.toString());
+        res.status(500).send("An error as occured");
+    }
 });
 
 userRouter.delete("/:id", async (req, res) => {
-
+    try {
+        const result = await pool.query("DELETE FROM users WHERE id=$1", [req.params.id]);
+        res.status(204).send();
+    } catch (error) {
+        console.error(error.toString());
+        res.status(500).send("An error occured");
+    }
 });
 
 
