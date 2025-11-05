479302bfa1f7751f981863e52bb9fc0ad0b9540f
Odiovock <64711277+Odiovock@users.noreply.github.com>
2025-11-04 20:04:32 -0500

    Sign Up page running localy

--- API/app.js
+++ API/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const cors = require("cors");
 
 const pool = require('./db');
 
@@ -12,6 +13,8 @@ const cartRoutes = require('./routes/cart');
 const productRoutes = require('./routes/product');
 const addtocartRoutes = require("./routes/addtocart");
 const checkoutRoutes = require("./routes/checkout");
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     try {
--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,10 @@
 import Root from "./components/root";
-import HomePage from "./pages/HomePage";
+import Authentication from "./pages/Authentication";
 import {RouterProvider, Route, createBrowserRouter, createRoutesFromElements} from "react-router-dom";
 
 const appRouter = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Root/>}>
-    <Route index element={<HomePage/>} />
+    <Route index element={<Authentication/>} />
   </Route>
 ));
 
